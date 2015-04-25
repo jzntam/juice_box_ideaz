043f88cc2862eea65f1d0c9b5b5745c93727748e
Jason <cameljeet@gmail.com>
2015-04-24 21:38:03 -0700

    Fixed Bugs in the Comments CRUD -Added User references to ideas and comments -installed simple form -created edit ideas view and edit comments view -added basic nav bar as a placeholder -formatted ideas#index view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,15 @@
 class CommentsController < ApplicationController
-  
-  before_action :find_idea
-  before_action :find_comment
+  # before_action :find_idea
+  # before_action :find_comment
 
   def index
-    
   end
 
   def create
-    @idea             = Idea.find params[:idea_id]
-    @comment          = Comment.new(comment_params)
-    @comment.user     = current_user
+    @idea         = Idea.find params[:idea_id]
+    @comment      = Comment.new(comment_params)
+    @comment.user = current_user
     @comment.idea = @idea
-    
     if @comment.save
       redirect_to idea_path(@idea), 
                     notice: "comment created!"
@@ -22,17 +19,19 @@ class CommentsController < ApplicationController
   end
 
   def edit
-
+    @idea = Idea.find params[:idea_id]
+    @comment = Comment.find params[:id]
   end
 
   def update
+    @idea = Idea.find params[:idea_id]
+    @comment = Comment.find params[:id]
     if @comment.update comment_params
       redirect_to @idea, notice: "Comment successfully updated"
     else
       render :edit
     end
   end
-
 
   def destroy
     @idea = Idea.find params[:idea_id]
@@ -42,18 +41,17 @@ class CommentsController < ApplicationController
                   notice: "comment deleted"
   end
 
-private
+  private
 
-  def find_idea
-    @idea = Idea.find params[:idea_id]
-  end
+  # def find_idea
+  #   @idea = Idea.find params[:idea_id]
+  # end
 
-  def find_comment
-    @comment = Comment.find params[:comment_id]
-  end
-
+  # def find_comment
+  #   @comment = Comment.find params[:id]
+  # end
 
   def comment_params
-      params.require(:comment).permit(:body)
+    params.require(:comment).permit(:body)
   end
 end
