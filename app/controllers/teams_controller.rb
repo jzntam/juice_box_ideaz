a30a21dfabf4453a5fd3dd2df4b1ad470b7841f3
Ciaran Lofts <cjlofts@sky.com>
2015-04-25 10:17:22 -0700

    adding 'team' crud

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,6 +1,11 @@
 class TeamsController < ApplicationController
+
   def index
-    @team = Team.all
+    @teams = current_user.teams
+  end
+
+  def new
+    @team = Team.new
   end
 
   def create
@@ -11,13 +16,28 @@ class TeamsController < ApplicationController
     end
   end
 
+  def show
+    @team = Team.find params[:id]
+  end
+
   def edit
+    @team = Team.find params[:id]
   end
 
   def update
+    @team = Team.find params[:id]
+    if @team.update(team_params)
+      redirect_to @team, notice: "Team Successfully Updated"
+    else
+      flash[:alert] = "Team Not Updated"
+      render :edit
+    end
   end
 
   def destroy
+    @team = Team.find params[:id]
+    @team.destroy
+    redirect_to user_path(current_user.id), notice: "Team Successfully Deleted"
   end
 
   private
