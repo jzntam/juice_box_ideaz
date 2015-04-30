5733be9e6d537fe73d7ac6d2a88dc1f7d3ca257a
Jason <cameljeet@gmail.com>
2015-04-30 00:39:40 -0700

    Logged in user will redirect to ideas_path automatically, not to the landing page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,15 @@
 class WelcomeController < ApplicationController
   layout "external"
+  before_action :logged_in?
   
   def index
+  end
+
+  private
+
+  def logged_in?
+    if current_user.present?
+      redirect_to ideas_path
+    end
   end
 end
