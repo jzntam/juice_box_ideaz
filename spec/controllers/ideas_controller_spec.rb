a6bbb3cfee1e907f4dfeb792aa442a984dbc7152
Jason <cameljeet@gmail.com>
2015-06-30 22:22:43 -0700

    Added comments to diff the describe and contexts

--- spec/controllers/ideas_controller_spec.rb
+++ spec/controllers/ideas_controller_spec.rb
@@ -8,7 +8,6 @@ RSpec.describe IdeasController, type: :controller do
   let(:idea_2) { create(:idea, user: user_1) }
 
   describe "#index" do
-
     context "user signed in" do
       before { login(user) }
       it "renders the index template" do
@@ -40,19 +39,16 @@ RSpec.describe IdeasController, type: :controller do
         # byebug
         # This test passes but needs work, pins
       end
-    end
-
+    end # context "with user signed in"
     context "user not signed in" do
       it "redirects to sign in page" do
         get :index
         expect(response).to redirect_to new_session_path
       end
-    end
-
-  end
+    end # context "user not signed in"
+  end # End of #index
 
   describe "#show" do
-
     context "with user signed in" do
       before {login(user)}
       before { get :show, id: idea.id }
@@ -68,19 +64,16 @@ RSpec.describe IdeasController, type: :controller do
       it "set a instance variable to a new share" do
         expect(assigns(:share)).to be_a_new Share
       end
-    end
-
+    end # context "with user signed in"
     context "user not signed in" do
       it "redirects to sign in page" do
         get :show, id: idea.id 
         expect(response).to redirect_to new_session_path
       end
-    end    
-  
-  end
+    end # context "user not signed in"
+  end # End of #show
 
   describe "#new" do
-
     context "with user signed in" do
       before {login(user)}
       it "renders the new template" do
@@ -91,16 +84,14 @@ RSpec.describe IdeasController, type: :controller do
         get :new
         expect(assigns(:idea)).to be_a_new(Idea)
       end
-    end
-
+    end # context "with user signed in"
     context "user not signed in" do
       it "redirects to sign in page" do
         get :new
         expect(response).to redirect_to new_session_path
       end
-    end
-    
-  end
+    end # context "user not signed in"
+  end # End of #New
 
   describe "#Create" do
     context "with user signed in" do
@@ -110,7 +101,6 @@ RSpec.describe IdeasController, type: :controller do
           post :create, {idea: {title: "Valid Title",
                                 description: "Valid Description"}}
         end
-
         it "instantiates a new variable with params passed in" do
           expect { valid_request }.to change { Idea.count }.by(1)
         end
@@ -136,8 +126,7 @@ RSpec.describe IdeasController, type: :controller do
           expect(response).to render_template(:new)
         end
       end
-    end
-
+    end # context "with user signed in"
     context "user not signed in" do
       context "with valid parameters" do
         def valid_request
@@ -159,7 +148,7 @@ RSpec.describe IdeasController, type: :controller do
           expect(response).to redirect_to new_session_path
         end
       end
-    end
-  end
+    end # context "user not signed in"
+  end # End of #create
 
 end
